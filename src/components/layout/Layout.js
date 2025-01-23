4683510f593a635f82584faa022932767cf723d0
Fish <fishagbalo@gmail.com>
2025-01-24 00:09:36 +0800

    Refactor Layout and NavigationCard components for improved responsiveness and styling

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -2,14 +2,20 @@ import React from "react";
 import NavigationCard from "./NavigationCard";
 
 function Layout({ children, hideNavigation }) {
+  let rightColumnClasses = "";
+  if (hideNavigation) {
+    rightColumnClasses += "w-full";
+  } else {
+    rightColumnClasses += "mx-4 md:mx-0 md:w-9/12";
+  }
   return (
-    <div className="flex mt-4 max-w-4xl mx-auto gap-6">
+    <div className="md:flex mt-4 max-w-4xl mx-auto gap-6">
       {!hideNavigation && (
-        <div className="w-3/12">
+        <div className="bottom-0 md:static fixed w-full md:w-3/12  -mb-5">
           <NavigationCard />
         </div>
       )}
-      <div className={hideNavigation ? "w-full" : "w-9/12"}>{children}</div>
+      <div className={rightColumnClasses}>{children}</div>
     </div>
   );
 }
--- src/components/layout/NavigationCard.js
+++ src/components/layout/NavigationCard.js
@@ -7,14 +7,14 @@ function NavigationCard() {
   const { pathname } = location;
   console.log(pathname);
   const activeElementClasses =
-    "flex gap-3 py-3 my-1 bg-socialBlue text-white -mx-10 px-10 rounded-md shadow-md shadow-gray-300";
+    "text-sm md:text-md flex md:gap-1 md:gap-3 py-3 my-1 bg-socialBlue text-white md:-mx-8 px-6 md:px-8 rounded-md shadow-md shadow-gray-300";
   const nonActiveElementClasses =
-    "flex gap-3 py-2 my-2 hover:bg-blue-500  hover:bg-opacity-20 -mx-4 px-4 rounded-md transition-all hover:scale-110 hover:shadow-md shadow-gray-300 ";
+    "text-sm md:text-md flex md:gap-1 md:gap-3 py-2 my-2 hover:bg-blue-500  hover:bg-opacity-20 md:-mx-4 px-6 md:px-4 rounded-md transition-all hover:scale-110 hover:shadow-md shadow-gray-300 ";
 
   return (
-    <Card>
-      <div className="px-4 py-2">
-        <h2 className="text-gray-400 mb-3">Navigation</h2>
+    <Card noPadding={true}>
+      <div className="px-4 py-2 flex justify-between md:block shadow-md shadow-gray-400 md:shadow-0 md:shadow-none">
+        <h2 className="text-gray-400 mb-3 hidden md:block">Navigation</h2>
         <Link
           to="/"
           className={
@@ -35,7 +35,7 @@ function NavigationCard() {
               d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
             />
           </svg>
-          Home
+          <span className="hidden md:block">Home</span>
         </Link>
         <Link
           to="/profile/friends"
@@ -59,7 +59,7 @@ function NavigationCard() {
               d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z"
             />
           </svg>
-          Friends
+          <span className="hidden md:block">Friends</span>
         </Link>
         <Link
           to="/saved"
@@ -83,7 +83,7 @@ function NavigationCard() {
               d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
             />
           </svg>
-          Saved Posts
+          <span className="hidden md:block">Saved Posts</span>
         </Link>
         <Link
           to="/notification"
@@ -107,7 +107,7 @@ function NavigationCard() {
               d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0M3.124 7.5A8.969 8.969 0 0 1 5.292 3m13.416 0a8.969 8.969 0 0 1 2.168 4.5"
             />
           </svg>
-          Notifications
+          <span className="hidden md:block">Notifications</span>
         </Link>
         <Link className={nonActiveElementClasses}>
           <svg
@@ -124,7 +124,7 @@ function NavigationCard() {
               d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75"
             />
           </svg>
-          Logout
+          <span className="hidden md:block">Logout</span>
         </Link>
       </div>
     </Card>
