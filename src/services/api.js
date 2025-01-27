30a7613bb88f124171f81e2c04f6665f3c698544
Fish <fishagbalo@gmail.com>
2025-01-27 23:22:34 +0800

    Add fetchPostById function to API service and comment out fetchProfile

--- src/services/api.js
+++ src/services/api.js
@@ -7,3 +7,5 @@ const api = axios.create({
 export const signup = (userData) => api.post("/signup", userData);
 export const login = (credentials) => api.post("/login", credentials);
 export const fetchPosts = () => api.get("/posts");
+// export const fetchProfile = (userId) => api.get(`/profile/${userId}`);
+export const fetchPostById = (postId) => api.get(`/posts/${postId}`);
